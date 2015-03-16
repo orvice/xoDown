f8d960ca0f240fa9d886cc6f6e651f4a5eda7e17
orvice <orvice@gmail.com>
2015-03-16 19:50:37 +0800

    update login pages

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,16 +51,17 @@
             <a href="#" class="btn btn-block btn-social btn-google-plus btn-flat"><i class="fa fa-google-plus"></i> Sign in using Google+</a>
         </div><!-- /.social-auth-links -->
 
-        <a href="{{ url('/password/email') }}" >忘记密码</a><br>
-        <a href="{{ url('/auth/register') }}" class="text-center">注册</a>
+        <a href="{{ url('/password/email') }}" >忘记密码</a>|<a href="{{ url('/auth/register') }}" class="text-center">注册</a>
 
     </div><!-- /.login-box-body -->
 </div><!-- /.login-box -->
 
 <!-- jQuery 2.1.3 -->
-<script src="../../plugins/jQuery/jQuery-2.1.3.min.js"></script>
+<script src="{{ asset('/js/jquery.min.js') }}"></script>
 <!-- Bootstrap 3.3.2 JS -->
-<script src="../../bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="{{ asset('/js/bootstrap.min.js') }}" type="text/javascript"></script>
+<!-- AdminLTE App -->
+<script src="{{ asset('/js/app.min.js') }}" type="text/javascript"></script>
 <!-- iCheck -->
 <script src="{{ asset('/plugins/iCheck/icheck.min.js') }}" type="text/javascript"></script>
 <script>
