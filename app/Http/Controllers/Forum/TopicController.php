65d8edb8f2c80ab114a9e0463adbdb8340a54d9d
orvice <orvice@gmail.com>
2015-04-01 15:13:58 +0800

    add topic

--- app/Http/Controllers/Forum/TopicController.php
+++ app/Http/Controllers/Forum/TopicController.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\Controller;
 
 use Illuminate\Http\Request;
 
-class TopicHomeController extends Controller {
+class TopicController extends Controller {
 
 	/**
 	 * Display a listing of the resource.
