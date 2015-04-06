5240b19daa1bd11e860c7963b152da2368a30769
orvice <orvice@gmail.com>
2015-04-07 01:51:08 +0800

    rm

--- resources/views/Home/home.blade.php
+++ resources/views/Home/home.blade.php
@@ -163,11 +163,7 @@
                 <!-- PRODUCT LIST -->
                 <div class="box box-primary">
                     <div class="box-header with-border">
-                        <h3 class="box-title">最近新闻</h3>
-                        <div class="box-tools pull-right">
-                            <button class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
-                            <button class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
-                        </div>
+                        <h3 class="box-title">最近新闻</h3> 
                     </div><!-- /.box-header -->
                     <div class="box-body">
                         <div class="table-responsive">
