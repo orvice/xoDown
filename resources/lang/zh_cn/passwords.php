eb8d4066780866984f5b9b7f753aa7f359aaa69c
orvice <orvice@gmail.com>
2015-05-09 16:29:07 +0800

    zh_cn

---
+++ resources/lang/zh_cn/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+	/*
+	|--------------------------------------------------------------------------
+	| Password Reminder Language Lines
+	|--------------------------------------------------------------------------
+	|
+	| The following language lines are the default lines which match reasons
+	| that are given by the password broker for a password update attempt
+	| has failed, such as for an invalid token or invalid new password.
+	|
+	*/
+
+	"password" => "密码太短或者2次输入符.",
+	"user" => "email不存在.",
+	"token" => "链接无效.",
+	"sent" => "已经发送密码重置链接!",
+	"reset" => "密码已经重置!",
+
+];
